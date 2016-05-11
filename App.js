40f96a4adf62cedd52c33c98c55a096a1c69f355
Stephen Dodd <stephen.dodd@rea-group.com>
2016-05-11 22:17:16 +1000

    Finish tute4 add input box and state variable to update text

--- App.js
+++ App.js
@@ -2,8 +2,23 @@ import React from 'react'
 
 //One way of declaring a component where the component can have state.
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { txt: 'this is the state text'}
+  }
+  update(e){
+    this.setState({
+      txt: e.target.value
+    })
+  }
   render() {
-    return <div>Hello world</div>
+    return (
+       <div>
+       <input type="text"
+          onChange={this.update.bind(this)} />
+        <h1>{this.state.txt}</h1>
+       </div>
+     )
   }
 }
 
@@ -11,3 +26,7 @@ class App extends React.Component {
 //const App = () => <h1>Hello world</h1>
 
 export default App
+
+//In this lesson we will learn about state
+//We can update our h1 label state in realtime through the input text box
+//As we type into the input our h1 tag is updated.
