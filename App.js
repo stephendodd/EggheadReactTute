da821893e62d871e2a0af872b6f6db7bcc2611cb
Stephen Dodd <stephen.dodd@rea-group.com>
2016-05-11 21:54:40 +1000

    Create egghead tutorial repo

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+//One way of declaring a component where the component can have state.
+class App extends React.Component {
+  render() {
+    return <div>Hello world</div>
+  }
+}
+
+//Another method for declaring a component where the component is stateless
+//const App = () => <h1>Hello world</h1>
+
+export default App
