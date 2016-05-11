89210a14f0145e69643d253a4b205c12b7fb6eea
Stephen Dodd <stephen.dodd@rea-group.com>
2016-05-11 22:10:03 +1000

    Add app prop types and default props as well as txt props variable to be rendered.

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
 //One way of declaring a component where the component can have state.
 class App extends React.Component {
   render() {
-    return <div>Hello world</div>
+    let txt=this.props.txt
+    return <h1>{txt}</h1>
   }
+}
+
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  txt: 'This is the default text'
 }
 
 //Another method for declaring a component where the component is stateless
 //const App = () => <h1>Hello world</h1>
+ReactDOM.render(
+  //<App cat={5} txt="this is the props value" />,
+  <App cat={5} />,
+  document.getElementById('app')
+);
 
-export default App
+//In this lesson we looked at App.defaultprops to define the default value of our props and App.PropTypes to define the types of our props
