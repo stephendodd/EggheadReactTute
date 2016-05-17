bedba7f5d4c9564990c5c5473907324ef50c14fd
Stephen Dodd <stephen.dodd@rea-group.com>
2016-05-17 11:27:44 +1000

    Finish tute 10

--- App.js
+++ App.js
@@ -1,13 +1,64 @@
-import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-//One way of declaring a component where the component can have state.
 class App extends React.Component {
-  render() {
-    return <div>Hello world</div>
+  constructor(){
+    super();
+    this.state = { val: 0 };
+    this.update = this.update.bind(this);
+  }
+  update(){
+    this.setState({val: this.state.val + 1 })
+  }
+  componentWillMount(){
+    this.setState({
+      mult: 2
+    })
+    console.log('mounting')
+  }
+  render(){
+    console.log('rendering!')
+    return (
+      <button onClick={this.update}>
+        {this.state.val * this.state.mult}</button>
+    )
+  }
+  componentDidMount(){
+    console.log(ReactDOM.findDOMNode(this));
+    console.log('mounted')
+    this.inc = setInterval(this.update,500)
+  }
+  componentWillUnmount(){
+    clearInterval(this.inc);
+    console.log('bye!')
   }
 }
 
-//Another method for declaring a component where the component is stateless
-//const App = () => <h1>Hello world</h1>
+class Wrapper extends React.Component {
+  constructor(){
+    super();
+  }
+  mount(){
+    ReactDOM.render(<App />, document.getElementById('a'))
+  }
+  unmount(){
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+  }
+  render(){
+    return (
+        <div>
+          <button onClick={this.mount.bind(this)}>Mount</button>
+          <button onClick={this.unmount.bind(this)}>Unmount</button>
+          <div id="a"></div>
+        </div>
+    )
+  }
+}
 
-export default App
+export default Wrapper
+
+//Looked at more examples of what we can do at each of the different component life cycle phases
+//In the component will mount we can set the state of some multiplier = 2, then in the render we can use this multiplier to output multiples of 2 each time the button is pressed and re-rendered
+//In the did mount we can findDOMNode and display the html we can then set an increment interval to run this.update
+//In the unmount we can clear the interval we set in didMount
+//It is important to clear the interval before in the unmount phase otherwise we are trying to update a component that has been unmounted.
