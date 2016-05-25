78369e9a821ad7bc3105c0c9c0a3bc388454bc9b
Stephen Dodd <stephen.dodd@rea-group.com>
2016-05-25 10:02:59 +1000

    Setup tute12

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
-//One way of declaring a component where the component can have state.
 class App extends React.Component {
-  render() {
-    return <div>Hello world</div>
-  }
-}
 
-//Another method for declaring a component where the component is stateless
-//const App = () => <h1>Hello world</h1>
+  constructor(){
+    super();
+    this.update = this.update.bind(this);
+    this.state = {val: 0}
+  }
+  update(){
+    this.setState({val: this.state.val + 1})
+  }
+  componentWillMount(){
+    console.log('will mount')
+  }
+
+  componentDidMount(){
+    console.log('mounted')
+  }
+
+  render(){
+    return (
+        <button onClick={this.update}>
+          {this.props.txt} - {this.state.val}
+        </button>
+    );
+  }
+
+}
+App
+
+App.defaultProps = {txt: 'button'}
 
 export default App
