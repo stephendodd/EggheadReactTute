7de262df426da0a25f7b18a6dc3dd96eb94e0350
Stephen Dodd <stephen.dodd@rea-group.com>
2016-05-25 10:43:08 +1000

    finish tute 13

--- App.js
+++ App.js
@@ -5,52 +5,75 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      red: 128,
-      green: 128,
-      blue: 128
+      red: 128
     }
     this.update = this.update.bind(this)
   }
   update(e) {
     this.setState({
-      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
-      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
-      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
+      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value
     })
   }
-  render(){
+  render() {
     return (
       <div>
-        <Slider ref="red" update={this.update} />
-        {this.state.red}
-        <br />
-        <Slider ref="green" update={this.update} />
-        {this.state.green}
-        <br />
-        <Slider ref="blue" update={this.update} />
-        {this.state.blue}
-        <br />
+        <NumInput
+          ref="red"
+          min={0}
+          max={255}
+          step={0.01}
+          val={+this.state.red}
+          label="Red"
+          type="range"
+          update={this.update} />
       </div>
     );
   }
 }
 
-class Slider extends React.Component {
+class NumInput extends React.Component {
   render() {
+    let label = this.props.label !== '' ?
+      <label>{this.props.label} - {this.props.val}</label> : ''
     return (
       <div>
-        <input ref="inp" type="range"
-          min="0" max="255"
-        onChange={this.props.update} />
+        <input
+          ref="inp"
+          type={this.props.type}
+          min={this.props.min}
+          max={this.props.max}
+          step={this.props.step}
+          defaultValue={this.props.val}
+          onChange={this.props.update} />
+        {label}
       </div>
     );
   }
+}
+
+NumInput.propTypes = {
+  min: React.PropTypes.number,
+  max: React.PropTypes.number,
+  step: React.PropTypes.number,
+  val: React.PropTypes.number,
+  label: React.PropTypes.string,
+  update: React.PropTypes.func.isRequired,
+  type: React.PropTypes.oneOf(['number', 'range'])
+}
+
+NumInput.defaultProps = {
+  min: 0,
+  max: 0,
+  step: 1,
+  val: 0,
+  label: '',
+  type: 'range'
 }
 
 export default App
 
 //What was created in this lesson
 
-//Three slider components that ranged from 0 to 255 were created to represent red green and blue.
-//The ref property was used to refer to a particular component
-//Set state was introduced to update the state of our App components properties of red, green and blue
+//Learned how to make our colour sliders more usable by using propTypes
+//Now we can convert out component from a range to a number just by changing the type
+//We can also change the min max and step
