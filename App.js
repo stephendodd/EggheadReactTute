396aff10c7903e41afed644bbb3dbbaee51f3fde
Stephen Dodd <stephen.dodd@rea-group.com>
2016-05-25 10:27:13 +1000

    Start of tute 13

--- App.js
+++ App.js
@@ -1,13 +1,56 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
-//One way of declaring a component where the component can have state.
 class App extends React.Component {
-  render() {
-    return <div>Hello world</div>
+  constructor() {
+    super();
+    this.state = {
+      red: 128,
+      green: 128,
+      blue: 128
+    }
+    this.update = this.update.bind(this)
+  }
+  update(e) {
+    this.setState({
+      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
+      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
+    })
+  }
+  render(){
+    return (
+      <div>
+        <Slider ref="red" update={this.update} />
+        {this.state.red}
+        <br />
+        <Slider ref="green" update={this.update} />
+        {this.state.green}
+        <br />
+        <Slider ref="blue" update={this.update} />
+        {this.state.blue}
+        <br />
+      </div>
+    );
   }
 }
 
-//Another method for declaring a component where the component is stateless
-//const App = () => <h1>Hello world</h1>
+class Slider extends React.Component {
+  render() {
+    return (
+      <div>
+        <input ref="inp" type="range"
+          min="0" max="255"
+        onChange={this.props.update} />
+      </div>
+    );
+  }
+}
 
 export default App
+
+//What was created in this lesson
+
+//Three slider components that ranged from 0 to 255 were created to represent red green and blue.
+//The ref property was used to refer to a particular component
+//Set state was introduced to update the state of our App components properties of red, green and blue
